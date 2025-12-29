eb8d29959f65b30d6bfa6143fe661d3263afd366
Gandhi Tech AI <kumarayshu852@gmail.com>
2025-12-29 14:10:38 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance=axios.create({
-    baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "https://chattifiy.vercel.app/api",
+    baseURL:"http://localhost:5001/api" : "https://chattifiy.vercel.app/api",
     withCredentials: true,
 });

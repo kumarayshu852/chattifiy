0d04a67b61af32a8f9cbe7ce6caef731b2346671
Gandhi Tech AI <kumarayshu852@gmail.com>
2025-12-29 14:07:13 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,14 +32,6 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
 
-if(process.env.NODE_ENV=== "production"){
-    app.use(express.static(path.join(_dirname,"../frontend/dist")));
-
-
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(_dirname,"../frontend","dist", "index.html"))
-    })
-}
 
 server.listen(PORT,()=>{
     console.log("server is running on PORT:" + PORT);

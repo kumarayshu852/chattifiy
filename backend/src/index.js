229a12809ec0c19358ade4bc81a2e2c5728149e3
Gandhi Tech AI <kumarayshu852@gmail.com>
2026-01-12 15:50:49 +0530

    Update index.js
    code changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,14 +29,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
 
-if(process.env.NODE_ENV=== "production"){
-    app.use(express.static(path.join(_dirname,"../frontend/dist")));
 
-
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(_dirname,"../frontend","dist", "index.html"))
-    })
-}
 
 server.listen(PORT,()=>{
     console.log("server is running on PORT:" + PORT);
